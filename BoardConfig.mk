51755c22e506a721e9c17f08d6a08031bbbb46a7
jayanth-coder <71193355+jayanth-coder@users.noreply.github.com>
2022-09-18 04:28:16 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,8 +29,7 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
-BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_KERNEL_CONFIG := sweet_defconfig
+
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld LLVM=1 LLVM_IAS=1
 TARGET_KERNEL_CLANG_COMPILE := true
 KERNEL_SUPPORTS_LLVM_TOOLS := true
@@ -44,8 +43,8 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
-KERNEL_LD := LD=ld.lld(shell pwd)/prebuilts/clang/host/linux-x86/clang-playground
-TARGET_KERNEL_ARCH := arm64
+KERNEL_LD := LD=ld.lld
+BOARD_KERNEL_IMAGE_NAME := Image
 
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 9126805504
